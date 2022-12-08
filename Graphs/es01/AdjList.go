b12e82b6f126cf477e3c315839bdaa24be6a27a0
Ioan Gabriel Dolca <116063534+Gavrijl@users.noreply.github.com>
2022-12-08 14:37:21 +0100

    Update AdjList.go
    Simple implementation of a graph which edges are integers

--- Graphs/es01/AdjList.go
+++ Graphs/es01/AdjList.go
@@ -1,1 +1,83 @@
+package main
+import (
+   "fmt"
+    "log"
+    "bufio"
+    "os"
+    "strconv"
+    "strings"
+)
 
+type graph struct {
+n int // numero di vertici
+adiacenti []* linkedList
+}
+type listNode struct {
+	item int
+	next *listNode
+}
+type linkedList struct {
+  head *listNode
+}
+
+func newNode(val int) *listNode {
+	node := new(listNode) 
+	(*node).item = val 
+	return node        
+}
+func NewGraph(n int) *graph{
+  var a []*linkedList
+  for i:=0;i<n;i++{
+    l := &linkedList{nil}
+    a=append(a,l)
+  }
+return &graph{n,a}
+}
+func addNewNode(l *linkedList, val int) {
+	node := newNode(val)
+	node.next = (*l).head
+	(*l).head = node
+}
+func NewEdge(g *graph,v1 int ,v2 int){
+addNewNode(g.adiacenti[v1],v2)
+}
+func printList(l linkedList) {
+	var p *listNode
+	p = l.head
+	for p != nil {
+		fmt.Print((*p).item, " ")
+		p = p.next
+	}
+}
+func PrintVertices(g *graph, v int){
+printList(*g.adiacenti[v])
+}
+
+func PrintGraph(g *graph,n int){
+  for i:=0;i<5;i++{
+  fmt.Print(i," è adiacente a ")
+  PrintVertices(g,i)
+  fmt.Println()
+  }
+}
+func main() {
+  f,err:= os.Open("input.txt")
+  if err!=nil{
+      log.Fatal(err)
+    }
+
+  defer f.Close()
+  scanner := bufio.NewScanner(f)
+  n,_:=strconv.Atoi(os.Args[1])
+  g:=NewGraph(n)
+  for scanner.Scan(){
+    if scanner.Text()==""{
+      break
+    }
+  arr:=strings.Split(scanner.Text()," ")
+  z,_:=strconv.Atoi(arr[0])
+  x,_:=strconv.Atoi(arr[1])
+  NewEdge(g,z,x)
+  }
+PrintGraph(g,n)
+}
